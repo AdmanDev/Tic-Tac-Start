c1d4c6e44ac6b539dcc8a0610798504f4b0783cd
ADMANSoftwareFR <49595294+ADMANSoftwareFR@users.noreply.github.com>
2020-06-15 17:56:24 +0200

    Release 2.3

---
+++ MainPlugin/PressKey/PressKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainPlugin.PressKey
+{
+    public static class PressKey
+    {
+        public static void Press(string keys)
+        {
+            System.Windows.Forms.SendKeys.SendWait(keys);
+        }
+    }
+}
---
+++ MainPlugin/PressKey/PressKey_ParamsDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace MainPlugin.PressKey
+{
+    public partial class PressKey_ParamsDialog : Window
+    {
+        public PressKey_ParamsDialog()
+        {
+            InitializeComponent();
+        }
+
+        public object[] GetParameters()
+        {
+            return new object[] { this.TB.Text };
+        }
+
+        private void BT_Validate_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void BT_Help_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "https://docs.microsoft.com/fr-fr/dotnet/api/system.windows.forms.sendkeys.send?redirectedfrom=MSDN&view=netcore-3.1#remarques";
+            MyFunctions.ProcessManager.RunPowerShellCommand("Start \"" + url + "\"");
+        }
+    }
+}
